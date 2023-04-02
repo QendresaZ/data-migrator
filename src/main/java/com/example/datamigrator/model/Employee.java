baa386bc11ee2cf55d1313643177bc7c41e35317
QendresaZ <qz40092@ubt-uni.net>
2023-04-02 18:01:11 +0200

    project init

---
+++ src/main/java/com/example/datamigrator/model/Employee.java
@@ -0,0 +1,4 @@
+package com.example.datamigrator.model;
+
+public class Employee {
+}
---
+++ src/test/java/com/example/datamigrator/DataMigratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datamigrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMigratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
